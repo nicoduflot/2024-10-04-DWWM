7a85858235f94a1d298898efac64e1ef7d40e650
unknown <nduflot@dawan.fr>
2024-10-04 14:40:07 +0200

    css root et un petit reset css homemade

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ use App\Jeuvideo;
     <meta name="keywords" content="Jeux vidéo, Vente, Achat, Console, Pc, retro gaming, retrogaming, rétrogamming" />
     <meta name="description" content="Venez vendre acheter et échanger vos jeux vidéos, console et PC, site spécialisé pour le rétro-gaming" />
     <title>Trader Gamers | Échangez - vendez vos jeux vidéos | Console - Pc</title>
-    <link rel="stylesheet" href="./assets/css/style.css" />
+    <link rel="stylesheet" href="./assets/css/style.css" media="all" />
     <script type="module" src="./assets/js/Index.js"></script>
 </head>
 <body>
